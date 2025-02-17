51c662d3ca5f14dc4f1677b0a836f448e3438ded
suzur <suzur@tanaka>
2025-02-17 21:18:25 +0900

    課題1-3 新規作成

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name = "田中　葵";
+		// 年齢
+		int age = 26;
+		// 趣味
+		String hobby = "カフェ巡り、ゲーム";
+		
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		// TODO 自動生成されたメソッド・スタブ
+	}
+}
