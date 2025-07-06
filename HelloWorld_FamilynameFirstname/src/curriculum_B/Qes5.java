bcebf829a3c77a2fc8934bee1205916583d15ea3
suzur <suzur@tanaka>
2025-07-06 21:25:20 +0900

    java課題18、修正分

--- HelloWorld_FamilynameFirstname/src/curriculum_B/Qes5.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_B/Qes5.java
@@ -2,25 +2,20 @@ package curriculum_B;
 
 public class Qes5 {
 	public static void main(String[] args) {
-		String num[] = { "001", "002", "003", "004", "005", "006", "007", "008", "009" };
-		String kukustr = null ;
-		System.out.println("###掛け算の九九をループで表示###");
-		for (int i = 0; i < num.length; i++) {
-			for (int j = 0; j < num.length; j++) {
+		String[] num = {
+			"001", "002", "003", "004", "005", "006", "007", "008", "009", "010",
+			"011", "012", "013", "014", "015", "016", "017", "018", "019", "020"
+		};
+
+		System.out.println("### 001×001 から 009×020 までの九九を表示 ###");
+
+		for (int i = 0; i < 9; i++) {  // 001〜009 まで
+			for (int j = 0; j < num.length; j++) {  // 001〜020 まで
 				int kuku = Integer.valueOf(num[i]) * Integer.valueOf(num[j]);
-				if (kuku <= 9) {
-					kukustr = "00" + kuku;
-				} else if(kuku <= 90) {
-						kukustr = "0" + kuku;
-				} else {
-				kukustr = String.valueOf(kuku);
-				}
-				if (Integer.valueOf(num[j]) < 9) {
-					System.out.print(num[i] + " × " + num[j] + " = " + kukustr + "||");
-				} else {
-					System.out.print(num[i] + " × " + num[j] + " = " + kukustr + "||\r\n");	
-				}
+				String kukuStr = String.format("%03d", kuku);
+				System.out.print(num[i] + " × " + num[j] + " = " + kukuStr + " || ");
 			}
+			System.out.println();  // 1段ごとに改行
 		}
 	}
 }
