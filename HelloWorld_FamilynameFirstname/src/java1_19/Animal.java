b05e827d6b86258e028ef3872402e8ec33871421
suzur <suzur@tanaka>
2025-07-28 23:57:38 +0900

    java19の初回提出分

--- HelloWorld_FamilynameFirstname/src/java1_19/Animal.java
+++ HelloWorld_FamilynameFirstname/src/java1_19/Animal.java
@@ -1,13 +1,26 @@
 package java1_19;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
 public class Animal {
-	
 	public static void main(String[] args) {
+
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		
+		@SuppressWarnings("unused")
+		Dog dog = new Dog();
+		System.out.println(dog.name);
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		
+		Dog dog2 = new Dog(1);
+		System.out.println(dog2.count);
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
-		
+		LocalDateTime nowDate = LocalDateTime.now();
+		System.out.println(nowDate);
+		@SuppressWarnings("unused")
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:sss");
+
 	}
+
 }
--- HelloWorld_FamilynameFirstname/src/java1_19/Dog.java
+++ HelloWorld_FamilynameFirstname/src/java1_19/Dog.java
@@ -2,11 +2,23 @@ package java1_19;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	
+	String name;
+
 	// Q2：フィールドに動物の数の変数を定義してください。
-	
+	int count;
+
+	public String dogName;
+
+	public int dogCount;
+
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	
+	//コンストラクタ
+	public Dog() {
+		this.name = "犬";
+	}
+
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	
+	public Dog(int count) {
+		this.count = count;
+	}
 }
